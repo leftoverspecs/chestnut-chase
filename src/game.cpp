1b81e8b0bc966da508705e96e4540299b3fe082c
Philipp Waehnert <philipp.waehnert@posteo.de>
2023-02-05 15:29:53 +0100

    Adds outer game loop

--- src/game.cpp
+++ src/game.cpp
@@ -80,102 +80,103 @@ int main(int argc, char *argv[]) {
     engine::Audio audio(44100, MIX_DEFAULT_FORMAT, 2, 128);
     engine::Music music(song, sizeof(song));
     engine::Font font(WIDTH, HEIGHT, test_font, sizeof(test_font));
-    game::Screen screen(WIDTH, HEIGHT);
     engine::Controller controller1(0);
     engine::Controller controller2(1);
-    game::Score score(font, WIDTH, HEIGHT);
-    game::Chestnuts chestnuts(score, WIDTH, HEIGHT);
-    game::Health player1_health(true, WIDTH, HEIGHT);
-    game::Health player2_health(false, WIDTH, HEIGHT);
-    game::Player player1(screen, controller1, player1_health, chestnuts, true, 0.0f, 0.0f, WIDTH, HEIGHT);
-    game::Player player2(screen, controller2, player2_health, chestnuts, false, WIDTH - 50.0f, 0.0f, WIDTH, HEIGHT);
-    player1.set_other(player2);
-    player2.set_other(player1);
-    game::Background background(WIDTH, HEIGHT);
-    game::Leaves leaves(WIDTH, HEIGHT);
+    game::Screen screen(WIDTH, HEIGHT);
 
-    bool quit = false;
-    long last = SDL_GetTicks64();
-    float exposure = 3.2f;
-    float gamma = 0.6f;
-    music.play(-1);
-    while (!quit) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                quit = true;
-                break;
-            case SDL_KEYUP:
-                switch (event.key.keysym.sym) {
-                case SDLK_ESCAPE:
+    bool exit_game = false;
+    while (!exit_game) {
+        game::Score score(font, WIDTH, HEIGHT);
+        game::Chestnuts chestnuts(score, WIDTH, HEIGHT);
+        game::Health player1_health(true, WIDTH, HEIGHT);
+        game::Health player2_health(false, WIDTH, HEIGHT);
+        game::Player player1(screen, controller1, player1_health, chestnuts, true, 0.0f, 0.0f, WIDTH, HEIGHT);
+        game::Player player2(screen, controller2, player2_health, chestnuts, false, WIDTH - 50.0f, 0.0f, WIDTH, HEIGHT);
+        player1.set_other(player2);
+        player2.set_other(player1);
+        game::Background background(WIDTH, HEIGHT);
+        game::Leaves leaves(WIDTH, HEIGHT);
+
+        bool again = false;
+        bool quit = false;
+        long last = SDL_GetTicks64();
+        float exposure = 3.2f;
+        float gamma = 0.6f;
+        music.play(-1);
+        while (!quit) {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                switch (event.type) {
+                case SDL_QUIT:
+                    again = false;
                     quit = true;
                     break;
-                case SDLK_KP_PLUS:
-                    gamma += 0.1f;
-                    std::cout << "gamma = " << gamma << '\n';
+                case SDL_KEYUP:
+                    switch (event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        again = false;
+                        quit = true;
+                        break;
+                    }
                     break;
-                case SDLK_KP_MINUS:
-                    gamma -= 0.1f;
-                    std::cout << "gamma = " << gamma << '\n';
-                    break;
-                case SDLK_PAGEUP:
-                    exposure += 0.1f;
-                    std::cout << "exposure = " << exposure << '\n';
-                    break;
-                case SDLK_PAGEDOWN:
-                    exposure -= 0.1f;
-                    std::cout << "exposure = " << exposure << '\n';
+                case SDL_CONTROLLERBUTTONUP:
+                case SDL_CONTROLLERBUTTONDOWN:
+                    if (player1.is_dead() || player2.is_dead()) {
+                        quit = true;
+                        again = true;
+                    }
                     break;
                 }
-                break;
             }
-        }
-        const long next = SDL_GetTicks64();
-        const long diff = next - last;
-        last = next;
-        {
-            font.clear();
+            const long next = SDL_GetTicks64();
+            const long diff = next - last;
+            last = next;
+            {
+                font.clear();
 
-            auto binding = screen.bind_as_target();
+                auto binding = screen.bind_as_target();
+                glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                player1.update(diff);
+                player2.update(diff);
+                const glm::vec2 mid = 0.5f * (player1.get_position() + player2.get_position());
+                background.update(mid.x);
+
+                if (!player1.is_dead() && !player2.is_dead()) {
+                    chestnuts.update(diff);
+                } else {
+                    glm::mat4 model(1.0f);
+                    model = glm::translate(model, glm::vec3(WIDTH / 2.0f - 300.0f, HEIGHT / 2.0f - 20.0f, 0.0f));
+                    model = glm::scale(model, glm::vec3(2.0f * font.get_width(), 2.0f * font.get_height(), 1.0f));
+                    font.write(model, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), "Game Over!");
+                }
+
+                background.draw();
+                player1.draw();
+                player2.draw();
+                chestnuts.draw();
+                leaves.draw();
+                player1_health.draw();
+                player2_health.draw();
+                score.write();
+                font.draw();
+            }
             glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            player1.update(diff);
-            player2.update(diff);
-            const glm::vec2 mid = 0.5f * (player1.get_position() + player2.get_position());
-            background.update(mid.x);
+            screen.update(diff);
+            screen.draw();
+            SDL_GL_SwapWindow(window);
 
-            if (!player1.is_dead() && !player2.is_dead()) {
-                chestnuts.update(diff);
-            } else {
-                glm::mat4 model(1.0f);
-                model = glm::translate(model, glm::vec3(WIDTH / 2.0f - 300.0f, HEIGHT / 2.0f - 20.0f, 0.0f));
-                model = glm::scale(model, glm::vec3(2.0f * font.get_width(), 2.0f * font.get_height(), 1.0f));
-                font.write(model, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), "Game Over!");
-            }
-
-            background.draw();
-            player1.draw();
-            player2.draw();
-            chestnuts.draw();
-            leaves.draw();
-            player1_health.draw();
-            player2_health.draw();
-            score.write();
-            font.draw();
+            //if (diff > 0) {
+            //    const float fps = 1000.0f / (diff);
+            //    SDL_SetWindowTitle(window, std::to_string(fps).c_str());
+            //}
         }
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        screen.update(diff);
-        screen.draw();
-        SDL_GL_SwapWindow(window);
-
-        //if (diff > 0) {
-        //    const float fps = 1000.0f / (diff);
-        //    SDL_SetWindowTitle(window, std::to_string(fps).c_str());
-        //}
+        if (!again) {
+            exit_game = true;
+        }
     }
 
     SDL_GL_DeleteContext(context);
